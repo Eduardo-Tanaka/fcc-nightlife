4df0b7393e21caac5a876392a9c4caa96ed9bba6
Eduardo <eduardo.s.tanaka@gmail.com>
2016-01-27 15:33:34 -0200

    .

--- config/login.js
+++ config/login.js
@@ -5,7 +5,7 @@ module.exports = function(passport) {
 	passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "https://basejump-eduardo-tanaka.c9users.io/auth/github/callback"
+        callbackURL: "https://freecodecamp-nightlife.herokuapp.com/auth/github/callback/"
         },
         function(accessToken, refreshToken, profile, done) {
             User.findOrCreate({ githubId: profile.id }, function (err, user) {
